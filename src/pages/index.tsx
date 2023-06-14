e2d252b0c3ecdd611fee49aeb6e14b385570bb89
Raphael Souza <raphael.pinheiro.souza@gmail.com>
2023-06-14 10:02:12 -0400

    adds index page

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import { Container, Group, Text, Title } from "@mantine/core";
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <Container>
+      <Title>Home</Title>
+      <Group>
+        <Link href={"/blips"}>
+          <Text>Blips</Text>
+        </Link>
+        <Link href="/projects">
+          <Text>Projects</Text>
+        </Link>
+      </Group>
+    </Container>
+  );
+};
+
+export default Home;
