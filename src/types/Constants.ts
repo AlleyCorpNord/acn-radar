c7b310adb9e682b50f2cbfb1e7ec99bd2876c288
Raphael Souza <raphael.pinheiro.souza@gmail.com>
2023-07-03 14:03:17 -0400

    reverse constant change

--- src/types/Constants.ts
+++ src/types/Constants.ts
@@ -1,4 +1,4 @@
 export const RepositoryUrl =
   "https://github.com/AlleyCorpNord/acn-radar/tree/main";
-export const BaseUrl = "http://localhost:3000";
+export const BaseUrl = "https://radar.alleycorpnord.com";
 export const CMSUrl = `${BaseUrl}/admin/#`;
